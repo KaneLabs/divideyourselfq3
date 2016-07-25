d6a7baad5f297a3f2a4ed2970409d531e9dc5361
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-07-25 11:26:00 -0600

    added tribe reference column

--- migrations/20160725100032_users_table.js
+++ migrations/20160725100032_users_table.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users',function(table) {
     table.increments();
+    table.integer('tribe_id').references('id').inTable('tribe');
     table.string('email').unique().notNullable();
     table.string('username').unique().notNullable();
     table.string('password').notNullable();
@@ -9,8 +10,6 @@ exports.up = function(knex, Promise) {
     table.string('firstname');
     table.string('lastname');
     table.string('profile_url').defaultTo('https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg');
-    // Reference to tribes table
-    // table.integer('tribe_id').references('id').inTable('tribe');
   });
 };
 
