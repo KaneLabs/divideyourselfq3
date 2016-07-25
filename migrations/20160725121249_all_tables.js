40e0ac3effc9265dc77e7123dfc7444a95580c0e
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-07-25 12:22:09 -0600

    consolidated migrations

---
+++ migrations/20160725121249_all_tables.js
@@ -0,0 +1,67 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    // Create 'users' table
+    knex.schema.createTable('users',function(table) {
+      table.increments();
+      table.integer('tribe_id').references('id').inTable('tribes');
+      table.string('email').unique().notNullable();
+      table.string('username').unique().notNullable();
+      table.string('password').notNullable();
+      table.boolean('isGod').defaultTo(false);
+      table.string('firstname');
+      table.string('lastname');
+      table.string('profile_url').defaultTo('https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg');
+    }),
+    // Create 'posts' table
+    knex.schema.createTable('posts', function(table) {
+      table.increments();
+      table.integer('user_id').notNullable().references('id').inTable('users');
+      table.string('title').notNullable();
+      table.string('type');
+      table.text('body').notNullable();
+      table.text('media_url').notNullable();
+      table.decimal('lat', 15, 3).notNullable();
+      table.decimal('lng', 15, 3).notNullable();
+      table.bigInteger('timestamp').notNullable();
+    }),
+    // Create 'comments' table
+    knex.schema.createTable('comments', function(table) {
+      table.increments();
+      table.integer('user_id').notNullable().references('id').inTable('users');
+      table.integer('post_id').notNullable().references('id').inTable('posts');
+      table.text('comment').notNullable();
+      table.bigInteger('timestamp').notNullable();
+    }),
+    // Create 'tribes' table
+    knex.schema.createTable('tribes', function(table) {
+      table.increments();
+      table.string('name').unique().notNullable();
+      table.text('description');
+    }),
+    // Create 'posts_votes' table
+    knex.schema.createTable('posts_votes',function(table) {
+      table.increments();
+      table.integer('post_id').references('id').inTable('posts');
+      table.integer('user_id').references('id').inTable('users');
+      table.integer('rating');
+    }),
+    // Create 'comments_votes' table
+    knex.schema.createTable('comments_votes',function(table) {
+      table.increments();
+      table.integer('comment_id').references('id').inTable('comments');
+      table.integer('rating');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.raw('drop table if exists users cascade'),
+    knex.raw('drop table if exists posts cascade'),
+    knex.raw('drop table if exists comments cascade'),
+    knex.raw('drop table if exists tribes cascade'),
+    knex.raw('drop table if exists posts_votes cascade'),
+    knex.raw('drop table if exists comments_votes cascade')
+  ]);
+};
--- seeds/users_seeds.js
+++ seeds/users_seeds.js
@@ -7,23 +7,28 @@ exports.seed = function(knex, Promise) {
         // Inserts seed entries
         knex('users').insert({
           username: 'ryan',
-          password: 'ryan'
+          password: 'ryan',
+          email: 'ryan@divideyourself.com'
         }),
         knex('users').insert({
           username: 'sean',
-          password: 'sean'
+          password: 'sean',
+          email: 'sean@divideyourself.com'
         }),
         knex('users').insert({
           username: 'brendan',
-          password: 'brendan'
+          password: 'brendan',
+          email: 'brendan@divideyourself.com'
         }),
         knex('users').insert({
           username: 'nathan',
-          password: 'nathan'
+          password: 'nathan',
+          email: 'nathan@divideyourself.com'
         }),
         knex('users').insert({
           username: 'conor',
-          password: 'conor'
+          password: 'conor',
+          email: 'conor@divideyourself.com'
         })
       ]);
     });
