1b5902ae5ef317333ef634709c6f511253862255
Act <thecanbeat@gmail.com>
2016-07-26 11:22:15 -0600

    show markers

--- public/js/app.js
+++ public/js/app.js
@@ -61,6 +61,17 @@ function LocationController($scope, $stateParams, $http, $rootScope){
         .then(data => {
           console.log(data);
           $scope.posts = data.data.posts;
+
+          data.data.posts.forEach(post => {
+            var latlng = {lat: parseFloat(post.lat), lng: parseFloat(post.lng)};
+            console.log(post.lat, post.lng);
+            console.log(latlng);
+            new google.maps.Marker({
+              position: latlng,
+              map: map,
+              title: post.title
+            });
+          });
         });
     });
   }
