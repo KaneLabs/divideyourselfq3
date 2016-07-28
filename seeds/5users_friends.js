1c586a3a2ebe34b3ac7b6e068c12e6431592bb8d
Nathan Musselman <nathanmuss@gmail.com>
2016-07-28 15:07:19 -0600

    Might have added an mvp tag?

--- seeds/5users_friends.js
+++ seeds/5users_friends.js
@@ -5,12 +5,12 @@ exports.seed = (knex, Promise) => {
     .then(() => {
       return Promise.all([
         // Inserts seed entries
-        knex('users_friends').insert({user_id: 1, friend_id: 3, friend_username: 'Brendan', profile_url: 'https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg'}),
-        knex('users_friends').insert({user_id: 2, friend_id: 3, friend_username: 'Brendan', profile_url: 'https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg'}),
-        knex('users_friends').insert({user_id: 1, friend_id: 3, friend_username: 'Brendan', profile_url: 'https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg'}),
-        knex('users_friends').insert({user_id: 3, friend_id: 1, friend_username: 'Ryan', profile_url: 'https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg'}),
-        knex('users_friends').insert({user_id: 3, friend_id: 2, friend_username: 'Sean', profile_url: 'https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg'}),
-        knex('users_friends').insert({user_id: 3, friend_id: 5, friend_username: 'Conor', profile_url: 'https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg'}),
+        knex('users_friends').insert({user_id: 1, friend_id: 3, friend_firstname: 'Brendan', profile_url: 'https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg'}),
+        knex('users_friends').insert({user_id: 2, friend_id: 3, friend_firstname: 'Brendan', profile_url: 'https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg'}),
+        knex('users_friends').insert({user_id: 1, friend_id: 3, friend_firstname: 'Brendan', profile_url: 'https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg'}),
+        knex('users_friends').insert({user_id: 3, friend_id: 1, friend_firstname: 'Ryan', profile_url: 'https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg'}),
+        knex('users_friends').insert({user_id: 3, friend_id: 2, friend_firstname: 'Sean', profile_url: 'https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg'}),
+        knex('users_friends').insert({user_id: 3, friend_id: 5, friend_firstname: 'Conor', profile_url: 'https://s3-us-west-2.amazonaws.com/divideyourself.com/images/1divide-logo.svg'}),
       ]);
     });
 };
