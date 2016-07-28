a2a444340f8f7c7cbd29d35600ce6453cadd238e
Nathan Musselman <nathanmuss@gmail.com>
2016-07-28 10:58:57 -0600

    Added seed points for comments, fixed upvote/downvoting, still can\'t get comment voting aligned right

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -177,14 +177,16 @@ function makeBodyController($scope, UsersService, apiInterceptor, NewCommentServ
   };
 
   $scope.upvote = (id, type, post) => {
+    console.log(type);
+    console.log(post);
     post.points += 1;
-    $http.post(`/theboard/upvote/${type}/${id}`)
+    $http.post(`/theboard/upvote/${type}/${post.id}`)
   };
 
   $scope.downvote = (id, type, post) => {
     post.points -= 1;
-    $http.post(`/theboard/downvote/${type}/${id}`)
+    $http.post(`/theboard/downvote/${type}/${post.id}`)
   };
-
+  
 };
 makeBodyController.$inject = ['$scope','UsersService', 'apiInterceptor', 'NewCommentService', "NewPostService","$http"];
