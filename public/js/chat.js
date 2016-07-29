fe9722fe01982b53fd21b40c813169e6f453380c
Act <thecanbeat@gmail.com>
2016-07-29 11:32:39 -0600

    fixed bugs with map and chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,62 +1,90 @@
-var x;
+var Magic = (number, callback) => {
+  var argumentArray = [];
+  return data => {
+    argumentArray.push(data);
+    if(argumentArray.length === number) return callback(argumentArray);
+  }
+};
+
+var magicCenter;
 
 app.factory("ChatService", () => $scope => {
+  if(!$scope.user) return;
   var host = location.origin.replace(/^http/, 'ws'),
-    chat = new WebSocket(host);
+    chat = {}
 
-  $scope.messages = localStorage.messages ? JSON.parse(localStorage.messages) : [];
+  $scope.messages = [];
 
-  chat.onmessage = e => {
-    var data = JSON.parse(e.data),
-      sender = $scope.messages.find(e => e.username === data.fromUser);
-    console.log(e.data);
-    if(data.connect) return console.log(data.message);
-    console.log(data);
-    if(data.fromUser && !sender){
-      sender = {
-        live: false,
-        username: data.fromUser,
-        log: []
-      };
+  startChat(chat);
+
+  function startChat(chat){
+    console.log("Starting Chat");
+    chat.socket = new WebSocket(host);
+    chat.socket.onopen = onOpen;
+    chat.socket.onmessage = onMessage;
+    chat.socket.onclose = () => startChat(chat);
+  }
+
+  function onOpen(){
+    console.log("Socket Opened");
+    if(!localStorage.center && !magicCenter) magicCenter = Magic(2, sendLocation);
+  }
+
+  function onMessage(e){
+    console.log("Got Data", e.data);
+    var data = JSON.parse(e.data);
+    if(data.fromUser === $scope.user.username) return;
+    var sender = $scope.messages.find(e => e.username === data.fromUser);
+    if(!sender && !data.connect){
+      sender = {live: false, username: data.fromUser, log: []};
       $scope.messages.push(sender);
     }
-    if(data.online){
-      sender.live = true;
-      if(data.center) sender.marker = new google.maps.Marker({
-        icon: "mapicons/dude.png",
-        position: JSON.parse(data.center),
-        map: map,
-        title: data.fromUser
-      });
-      else console.log(data);
-    }
-    if(data.offline){
-      sender.live = false;
-      if(sender.marker){
-        sender.marker.setMap(null);
-        sender.marker = null;
-      }
-    }
-    if(data.message) sender.log.push({
-      fromUser: data.fromUser,
-      message: data.message,
-      timestamp: data.timestamp
-    });
+    if(data.connect) dataConnect(data);
+    if(data.online) dataOnline(data, sender);
+    if(data.offline) dataOffline(sender);
+    if(data.message) dataMessage(data, sender);
     $scope.$apply();
   }
 
-  chat.onopen = () => {
-    if(!localStorage.center || !$scope.user) return;
-    chat.send(JSON.stringify({
-      connect: true,
-      fromUser: $scope.user.username,
-      center: localStorage.center
-    }));
-    $scope.$apply();
-  };
+  function dataConnect(data){
+    console.log("Connected to Server");
+    magicCenter ? magicCenter() : sendLocation();
+  }
 
-  var service = {
+  function dataOnline(data, sender){
+    sender.live = true;
+    sender.marker = new google.maps.Marker({
+      icon: "mapicons/dude.png",
+      position: typeof data.center === "string" ? JSON.parse(data.center) : data.center,
+      map: map,
+      title: data.fromUser
+    });
+  }
+
+  function dataOffline(sender){
+    console.log("User Offline:", sender.username);
+    sender.live = false;
+    if(sender.marker){
+      sender.marker.setMap(null);
+      sender.marker = null;
+    }
+  }
+
+  function dataMessage(data, sender){
+    console.log("Got Message:", data);
+    var message = {fromUser: data.fromUser, message: data.message, timestamp: data.timestamp};
+    sender.log.push(message);
+  }
+
+  function sendLocation(){
+    console.log("Sending Location");
+    var message = {connect: true, fromUser: $scope.user.username, center: JSON.parse(localStorage.center)};
+    chat.socket.send(JSON.stringify(message));
+  }
+
+  return {
     send: convo => {
+      console.log("Sending Data", message);
       var toUser = convo.username,
         message = convo.input,
         conversation = $scope.messages.find(e => e.username === toUser),
@@ -64,14 +92,10 @@ app.factory("ChatService", () => $scope => {
         timestamp = Date.now();
       if(!conversation || !conversation.live) return console.log(`User '${toUser}' is offline`);
       conversation.log.push({fromUser, message, timestamp});
-      chat.send(JSON.stringify({toUser, fromUser, message, timestamp}));
+      chat.socket.send(JSON.stringify({toUser, fromUser, message, timestamp}));
       convo.input = "";
     }
   };
-  x = service;
-  return service;
 });
 
-app.directive("chat", () => ({
-  templateUrl: "partials/chat.html"
-}));
+app.directive("chat", () => ({templateUrl: "partials/chat.html"}));
