e0de4d81e657daeb14f9d53c337d05163f6eabcd
Nathan Musselman <nathanmuss@gmail.com>
2016-07-28 23:11:51 -0600

    tribe routes ready

--- routes/tribes.js
+++ routes/tribes.js
@@ -9,7 +9,24 @@ router.get('/', (req, res) => {
   .then( (data) => {
     res.json(data)
   })
+});
+
+router.get('/:id', (req, res) => {
+  // Get my all users in my tribe
+});
+
+router.get('/join/:id', (req, res) => {
+  //Join tribe
+});
+
+router.get('/leave/:id', (req, res) => {
+  //Leave tribe
+});
+
+router.delete('/delete', (req, res) => {
+  // Authentice as tribe user and delete tribe
 })
+
 
 
 module.exports = router;
